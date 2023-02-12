a1daced689bd12768cc9e9b4b10d15a5c557540e
MrTban <esteman214@gmail.com>
2023-02-12 15:12:45 -0500

    first commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,19 +2,11 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const routerVideogames = require('./videogames');
-const routerGenres = require('./genres');
-const routerPlatforms = require('./platforms');
-// const videogameDetail = require('./videogameDetail');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use('/videogames', routerVideogames);
-router.use('/genres', routerGenres);
-router.use('/platforms', routerPlatforms);
-// router.use('/allvideogames', videogameDetail);
 
 module.exports = router;
