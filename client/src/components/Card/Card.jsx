b25daa6fff417e553a3aeda05eb25a6fad01d50c
MrTban <esteman214@gmail.com>
2023-02-23 16:57:25 -0500

    css fix

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,43 +5,39 @@ import defaultImg from '../../assets/imagen.jpg';
 
 const Card = ({ id, name, image, genres, rating }) => {
 	return (
-		<div id={id} key={id} className={style.all}>
-			<div className={style.html}>
-				<div className={style.body}>
-					<figure className={style.imageBlock}>
-						<img src={image ? image : defaultImg} alt={name} />
-						<figcaption>
-							<h3>
-								<b>
-									<i>{name}</i>
-								</b>
-							</h3>
-							<h4>
-								<i>{rating} &#9733;</i>
-							</h4>
-							<h3>
-								<i>
-									{genres && typeof genres[0] === 'object'
-										? genres?.map((g) => g.name + ', ')
-										: genres?.join(', ')}
-								</i>
-							</h3>
-							{/* <h4>
+		<div className={style.bodyCard}>
+			<figure className={style.imageBlock}>
+				<img src={image ? image : defaultImg} alt={name} />
+				<figcaption>
+					<h3>
+						<b>
+							<i>{name}</i>
+						</b>
+					</h3>
+					<h4>
+						<i>{rating} &#9733;</i>
+					</h4>
+					<h3>
+						<i>
+							{genres && typeof genres[0] === 'object'
+								? genres?.map((g) => g.name + ', ')
+								: genres?.join(', ')}
+						</i>
+					</h3>
+					{/* <h4>
 								{genres && (
 									<i>
 										<p>{genres.map((g) => Object.values(g))}</p>
 									</i>
 								)}
 							</h4> */}
-							<Link to={`/detail/${id}`} className={style.linkbutton}>
-								<button>
-									<i>Details</i>
-								</button>
-							</Link>
-						</figcaption>
-					</figure>
-				</div>
-			</div>
+					<Link to={`/detail/${id}`} className={style.linkbutton}>
+						<button>
+							<i>Details</i>
+						</button>
+					</Link>
+				</figcaption>
+			</figure>
 		</div>
 	);
 };
