30e1336d3af46b837eded229a34e4541eb7f2ebd
MrTban <esteman214@gmail.com>
2023-02-25 21:39:53 -0500

    cors update

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,9 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
 	// res.header('Access-Control-Allow-Origin', '*');
 
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header('Access-Control-Allow-Origin', 'https://pi-videogames-wheat.vercel.app');
+
+	// res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 
 	//res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
